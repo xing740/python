75bd20a82007ab0a7b86703b70ef8fdbf061fa78
xing <xxx@xxx.xxx>
2019-12-23 11:12:06 +0800

    //xpython

---
+++ csvn.py
@@ -0,0 +1,26 @@
+#!/user/bin/env python
+#coding:utf-8
+
+import os
+import sys
+note = sys.argv[1] #注释
+
+r = os.popen('svn stat')
+info = r.readlines()
+allFile = ''
+for l in info:
+    l = l.strip('\n').split('       ')
+    stat = l[0]
+    fileName = l[1]
+    #print "stat:" + stat + " fn:" + fileName
+    if (stat != 'M') or (fileName == 'task_checker.h'):
+        continue
+    allFile += ''
+    allFile += fileName
+
+print "allFile: " + allFile
+
+if allFile != '':
+    os.system("svn commit -m %s %s" % (note, allFile))
+
+
