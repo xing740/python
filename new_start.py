698c6424a9fe9b4264090c4995f5ff7ad0d96362
xing740 <752111707@qq.com>
2021-05-01 17:43:30 +0800

    //ad

--- new_start.py
+++ new_start.py
@@ -9,13 +9,119 @@ import multiprocessing
 import psutil
 import pyautogui as ai
 from pynput.keyboard import Listener
+from PIL import Image
 
+item_id_interval = 1000
+wu_qi = 1
+zhuan_bei = 2
+fu_shou = 3
+other = 4
+empty_id = 5
+error_id = 6
+itemDir = "items"
+tmp_item_name = "9999.png"
+empty_name = "10000.png"
+valMap = {}
+ids = []
+
+def CalSameVal():
+    image1 = Image.open("%s\%s" %(itemDir, tmp_item_name))
+    image1 = make_regalur_image(image1)
+    for it in ids:
+        if it == tmp_item_name:
+            continue
+        #image2 = Image.open("%s\%s" %(itemDir, it))
+        #image2 = make_regalur_image(image2)
+        image2 = valMap.get(it)
+        if image2 == None:
+            continue
+        val = calc_similar(image1, image2)
+        if val >= 0.65:
+            print("val:"+ str(val)+" itemId:"+str(it))
+            return int(it[0]) if it != empty_name else empty_id
+    return error_id
+    kk
+def wear():
+    changeBack(other)
+    ai.click(113,204)
+    time.sleep(0.5)
+    ai.click(113,287)
+    time.sleep(0.5)
+    ai.click(291,200)
+
+def changeBack(itemType):
+    ai.click(151, 271)
+    time.sleep(0.5)
+    if itemType == wu_qi:
+        ai.click(143, 350)
+    elif itemType == zhuan_bei:
+        ai.click(143, 325)
+    elif itemType == fu_shou:
+        ai.click(143, 300)
+    else:
+        ai.click(143, 375)
+def JudgeType():
+    x = 1560
+    y = 524
+    ix = 55
+    iy = 49
+    t = 82
+    old_type = 0
+    while True:
+        ai.click(1814, 452)
+        time.sleep(1)
+        for i in range(5):
+            for j in range(4):
+                posx = x + j * t
+                posy = y + i * t
+                ai.screenshot("%s\%s" %(itemDir, tmp_item_name), region=(posx, posy, ix, iy))
+                item_type = CalSameVal()
+                if item_type == error_id:
+                    continue
+                if item_type == empty_id:
+                    ai.keyUp("shift")
+                    return
+                print("type:"+str(item_type))
+                if item_type != old_type:
+                    changeBack(item_type)
+                    old_type = item_type
+                ai.keyDown("shift")
+                ai.moveTo(posx + ix / 2, posy + iy / 2)
+                ai.mouseDown()
+                ai.mouseUp()
+                ai.mouseDown()
+                ai.mouseUp()
+                ai.keyUp("shift")
+def initMap():
+    tmp = os.listdir(itemDir)
+    for it in tmp:
+        ids.append(it)
+        if it == tmp_item_name:
+            continue
+        image2 = Image.open("%s\%s" %(itemDir, it))
+        valMap[it] = make_regalur_image(image2)
+    print(valMap)
 
 def randomFloat(num, half=0):
     if half == 0:
         half = num / 2
     return random.uniform(num - half, num + half)
 
+# 将图片转化为RGB
+def make_regalur_image(img, size=(64, 64)):
+    gray_image = img.resize(size).convert('RGB')
+    return gray_image
+ 
+# 计算直方图
+def hist_similar(lh, rh):
+    assert len(lh) == len(rh)
+    hist = sum(1 - (0 if l == r else float(abs(l-r))/max(l,r))for l, r in zip(lh, rh))/len(lh)
+    return hist
+ 
+# 计算相似度
+def calc_similar(li, ri):
+    calc_sim = hist_similar(li.histogram(), ri.histogram())
+    return calc_sim
 
 def randomInt(num, half=0):
     if half == 0:
@@ -91,7 +197,8 @@ def judgeImageExists(image):
 
 
 def fouceFindImageClick(image, clickPos):
-    judgeImageExists(image)
+    #judgeImageExists(image)
+    time.sleep(0.4)
     clickImage(ai.locateOnScreen(image), clickPos)
 
 
@@ -101,7 +208,7 @@ class MainWindom:
         self._win.wm_attributes('-topmost', 1)
         self._win.title("助手")
         # 大小
-        self._win.geometry('300x200+0+0')
+        self._win.geometry('350x200+0+0')
         # 按钮
         #b1 = tkinter.Button(self._win, text="连点", command=self.click)
         b1 = tkinter.Button(self._win, text="清空背包", command=self.doMove)
@@ -112,6 +219,18 @@ class MainWindom:
 
         b3 = tkinter.Button(self._win, text="连点", command=self.doClick)
         b3.pack(side='left', padx=20)
+        #截图
+        b4 = tkinter.Button(self._win, text="截副手", command=lambda:self.jieTu(fu_shou))
+        b4.pack(side='bottom')
+
+        b5 = tkinter.Button(self._win, text="截武器", command=lambda:self.jieTu(wu_qi))
+        b5.pack(side='bottom')
+
+        b6 = tkinter.Button(self._win, text="截装备", command=lambda:self.jieTu(zhuan_bei))
+        b6.pack(side='bottom')
+
+        b7 = tkinter.Button(self._win, text="截其它", command=lambda:self.jieTu(other))
+        b7.pack(side='bottom')
 
         self._win.mainloop()
 
@@ -129,23 +248,45 @@ class MainWindom:
             time.sleep(float(CfgMgr.shared()._click_interval_tm))
 
     def doMove(self):
-        fx = CfgMgr.shared()._from_x
-        fy = CfgMgr.shared()._from_y
-        tx = CfgMgr.shared()._to_x
-        ty = CfgMgr.shared()._to_y
-        ix = CfgMgr.shared()._interval_x
-        iy = CfgMgr.shared()._interval_y
-        for y in range(0, 3):
-            for x in range(0, 3):
-                ai.moveTo(fx + ix * x, fy + iy * y, 0.2)
-                ai.dragTo(tx, ty, 0.2, button='left')
+        initMap()
+        JudgeType()
+        wear()
 
     def doSell(self):
         while True:
-            fouceFindImageClick(CfgMgr.shared()._sell_name, "centre")
-            fouceFindImageClick(CfgMgr.shared()._sell_type, "centre")
-            fouceFindImageClick(CfgMgr.shared()._sell_yes, "centre")
+            ai.click(1282,428)
+            time.sleep(0.3)
+            ai.click(864,513)
+            time.sleep(0.3)
+            ai.click(1149,730)
+            time.sleep(0.3)
+        #while True:
+            #fouceFindImageClick(CfgMgr.shared()._sell_name, "centre")
+            #fouceFindImageClick(CfgMgr.shared()._sell_type, "centre")
+            #fouceFindImageClick(CfgMgr.shared()._sell_yes, "centre")
 
+    def jieTu(self, typeId):
+        typeId *= item_id_interval
+        x = 1560
+        y = 524
+        ix = 55
+        iy = 49
+        t = 82
+        image1 = Image.open("%s\%s" %(itemDir, empty_name))
+        image1 = make_regalur_image(image1)
+        for i in range(5):
+            for j in range(4):
+                posx = x + j * t
+                posy = y + i * t
+                while os.path.exists("%s/%s.png" %(itemDir, typeId)):
+                    typeId+=1
+                newPath = itemDir + "/" + str(typeId) + ".png"
+                ai.screenshot(newPath, region=(posx, posy, ix, iy))
+
+                image2 = Image.open(newPath)
+                image2 = make_regalur_image(image2)
+                if calc_similar(image1, image2) > 0.7:
+                    os.remove(newPath)
 
 class PauseProcess(object):
     def __init__(self):
