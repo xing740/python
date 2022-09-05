83d1b579ba244d0721c9346e1c35b5e563d0a04e
xing <752111707@qq.com>
2022-09-05 14:42:22 +0800

    pos_help

--- pos_help.py
+++ pos_help.py
@@ -1,9 +1,21 @@
+from turtle import position
+
 import pyautogui as ai
 from pynput.mouse import Listener
 
+x = 0
+y = 0
 def do(a1, a2, a3, a4):
-    print(ai.position())
+    global x
+    global y
+    pos = ai.position()
+    print(pos)
+    print(str(pos.x - x) + " " + str(pos.y - y))
+    x = pos.x
+    y = pos.y
 
 if __name__ == '__main__':
     with Listener(on_click=do) as l:
         l.join()
+
+\\94 36
